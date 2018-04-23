e6b0f9445154062288e84ecbcc382c3f4c148cd2
Stu Ratcliffe <email@sturatcliffe.co.uk>
2018-04-24 00:52:45 +0100

    Setup and config

--- ClientApp/boot.js
+++ ClientApp/boot.js
@@ -5,23 +5,27 @@ import BootstrapVue from "bootstrap-vue";
 import NProgress from "nprogress";
 import VueToastr from "@deveodk/vue-toastr";
 import "@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css";
+import VeeValidate from "vee-validate";
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(VueToastr, {
   defaultPosition: "toast-top-right"
 });
+Vue.use(VeeValidate);
 
 // filters
-import { currency } from "./filters";
+import { currency, date } from "./filters";
 
 Vue.filter("currency", currency);
+Vue.filter("date", date);
 
 //import page components
 import Catalogue from "./pages/Catalogue.vue";
 import Product from "./pages/Product.vue";
 import Cart from "./pages/Cart.vue";
 import Checkout from "./pages/Checkout.vue";
+import Account from "./pages/Account.vue";
 
 import axios from "axios";
 
@@ -41,6 +45,7 @@ const routes = [
   { path: "/products/:slug", component: Product },
   { path: "/cart", component: Cart },
   { path: "/checkout", component: Checkout, meta: { requiresAuth: true } },
+  { path: "/account", component: Account, meta: { requiresAuth: true } },
   { path: "*", redirect: "/products" }
 ];
 
